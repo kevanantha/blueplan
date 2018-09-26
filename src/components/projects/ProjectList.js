2304e9dc4a6ad2216ee4e3dcfebb266cf5f90bfa
kananthav <kevin.anantha@gmail.com>
2018-09-26 17:05:49 +0800

    Dasboard component

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-contet grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Kevin Anantha</p>
+          <p className="grey-text">3rd Sep, 2am</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-contet grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Kevin Anantha</p>
+          <p className="grey-text">3rd Sep, 2am</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-contet grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Kevin Anantha</p>
+          <p className="grey-text">3rd Sep, 2am</p>
+        </div>
+      </div>
+
+    </div>
+
+  )
+}
+
+export default ProjectList;
