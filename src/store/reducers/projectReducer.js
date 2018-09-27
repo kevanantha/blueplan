13d081a6409afff6c61c8c3aafa443cd52cb83f5
kananthav <kevin.anantha@gmail.com>
2018-09-28 00:01:08 +0800

    Add dummy data

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-
+  projects: [
+    { id: '1', title: 'title 1', content: 'content 1 blah blah' },
+    { id: '2', title: 'title 2', content: 'content 2 blah blah' },
+    { id: '3', title: 'title 3', content: 'content 3 blah blah' }
+  ]
 }
 
 const projectReducer = (state = initState, action) => {
