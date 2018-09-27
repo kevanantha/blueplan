72b1da37b0ff2c684e97fb3042631fe308fe994d
kananthav <kevin.anantha@gmail.com>
2018-09-27 23:36:30 +0800

    Setup Redux and Reducers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import rootReducers from './store/reducers/rootReducer'
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducers);
+
+ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+import authReducer from './authReducer';
+import projectReducer from './projectReducer';
+import { combineReducers } from 'redux';
+
+const rootReducers = combineReducers({
+  auth: authReducer,
+  project: projectReducer
+});
+
+export default rootReducers;
