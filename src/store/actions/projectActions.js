c078eb7a602d8e46ff554f2f09b0a63766eba485
kananthav <kevin.anantha@gmail.com>
2018-10-03 17:55:11 +0800

    Add data to Firestore :fire:

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,9 +1,17 @@
 export const createProject = (project) => {
   return (dispatch, getState, {getFirebase, getFirestore}) => {
     // make async calll to db
-    dispatch({
-      type: 'CREATE_PROJECT',
-      project
+    const db = getFirestore();
+    db.collection('projects').add({
+      ...project,
+      authorFirstName: 'Kevin',
+      authorLastName: 'Anantha',
+      authorId: 123456789,
+      createdAt: new Date()
+    }).then(() => {
+      dispatch({ type: 'CREATE_PROJECT', project })
+    }).catch((err) => {
+      dispatch({ type: 'CREATE_PROJECT', err})
     })
   }
 }
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -10,8 +10,14 @@ const projectReducer = (state = initState, action) => {
   switch (action.type) {
     case 'CREATE_PROJECT':
       console.log('created project ', action.project)
+      alert('The project was successfully created')
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('create project error', action.err)
+      return state;
+    default:
+      return state;
   }
-  return state;
 }
 
 export default projectReducer;
